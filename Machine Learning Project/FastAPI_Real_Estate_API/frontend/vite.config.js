70be8322e98270d8e5b00d8b9d3e588fde05c18d
BantPawan <155041492+BantPawan@users.noreply.github.com>
2025-10-04 22:54:49 +0530

    Update vite.config.js

--- Machine Learning Project/FastAPI_Real_Estate_API/frontend/vite.config.js
+++ Machine Learning Project/FastAPI_Real_Estate_API/frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Changed from '/' to './'
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
