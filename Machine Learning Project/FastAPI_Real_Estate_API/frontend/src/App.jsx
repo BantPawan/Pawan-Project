bfb785ccceec4e2a2c02905ad1713b198438bb32
BantPawan <155041492+BantPawan@users.noreply.github.com>
2025-10-04 19:37:17 +0530

    Update App.jsx

--- Machine Learning Project/FastAPI_Real_Estate_API/frontend/src/App.jsx
+++ Machine Learning Project/FastAPI_Real_Estate_API/frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'react-plotly.js/dist/plotly-basic.css';  // Plotly styling
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import TechStack from './components/TechStack';
